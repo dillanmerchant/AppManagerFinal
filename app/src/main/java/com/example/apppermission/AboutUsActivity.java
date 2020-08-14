a3c60ebbe9bf0c7892c328e50da4e44bb4431420
dillanmerchant <60758198+dillanmerchant@users.noreply.github.com>
2020-08-13 22:49:48 -0700

    About Us fix

--- app/src/main/java/com/example/apppermission/AboutUsActivity.java
+++ app/src/main/java/com/example/apppermission/AboutUsActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.style.ImageSpan;
 import android.view.View;
+import android.view.textclassifier.TextLinks;
+import android.webkit.WebView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.apppermission.databutton.DataActivity;
 
@@ -16,6 +21,20 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        /*TextView paragraph = (TextView) findViewById(R.id.about_paragraph);
+        SpannableStringBuilder ssb = new SpannableStringBuilder();
+        ssb.append("AppManager app and ");
+        ssb.setSpan(new ImageSpan(this, R.drawable.ic_home_header_logo),
+                ssb.length()-1,
+                ssb.length(),
+                0);
+        ssb.append("Logo are the trademarks of SoftClouds LLC All rights reserved");
+        paragraph.setText(ssb);*/
+
+        WebView webView = findViewById(R.id.softcloud_links);
+        webView.loadUrl("file:///android_asset/ABOUT_US.html");
+
 
         ImageView backArrow = (ImageView) findViewById(R.id.about_back_arrow);
 
